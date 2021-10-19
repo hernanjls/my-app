3cc8348f121d31380b88c65f54b62ff1e217f399
hernanjls <hernanjls@gmail.com>
2021-10-19 16:14:22 -0400

    Nombre del Proyecto: my-app, Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/hernanjls/myapp/entity/TestEntity.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.hernanjls.myapp.entity;
+
+/**
+ * @author Hernan Lagrava Sanchez
+ *
+ */
+public class TestEntity {
+
+	private String nombre;
+
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
